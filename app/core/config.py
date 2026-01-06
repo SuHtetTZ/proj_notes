3a59187ebf1bc4e11aa8e3bd0c33bf45bf48c466
SuHtetTZ <thinzarchen@gmail.com>
2026-01-06 23:46:30 +0630

    Updated

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "mysql+pymysql://root:p3I%27sSQL@localhost:3306/fast_db"
+    DATABASE_URL: str = "mysql+pymysql://root:password@localhost:3306/fast_db"
     SECRET_KEY: str = "something here"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
